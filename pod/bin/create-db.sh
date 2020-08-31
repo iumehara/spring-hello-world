fdc19ca7a67a9bc9c0806dc0e603297f178af249
Ichizo Umehara <iumehara10@gmail.com>
2020-08-31 10:17:18 +0900

    Add volume config for tks

--- pod/bin/create-db.sh
+++ pod/bin/create-db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 kubectl create -f pod/db/db-configmap-definition.yaml
-kubectl create -f pod/db/db-volume-definition.yaml
+kubectl create -f pod/db/db-volume-tks.yaml
 kubectl create -f pod/db/db-deployment-definition.yaml
 kubectl create -f pod/db/db-service-definition.yaml
 kubectl get pvc postgres-pvc
 kubectl get svc postgres
-minikube ip
